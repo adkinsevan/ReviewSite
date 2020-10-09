aea5ab30c29aab50cf96ee6c8668c1b4e663ac06
Evan Adkins <adkinsevan@ymail.com>
2020-10-09 11:05:48 -0400

    this will have to do

--- src/main/java/com/reviewsite/demo/ReviewRepository.java
+++ src/main/java/com/reviewsite/demo/ReviewRepository.java
@@ -14,22 +14,22 @@ public class ReviewRepository {
     public ReviewRepository(){
         Review enterTheWuTang = new Review(1L,"Enter the Wu-Tang (36 Chambers)",
                 "Enter the Wu-Tang (36 Chambers) is the debut studio album by American hip hop group Wu-Tang Clan",
-                "1993", "Hardcore Hip-hop", "/resources/Images/36 chambers.jpeg",
+                "1993", "Hardcore Hip-hop", "/Images/36 chambers.jpeg",
                 "The most iconic hip-hop album of all time, period. Every song is a memorable hit that not only reshaped the genre itself, but, the entirety of the future of music.  It is the music that begun a new era, everything from the skits, to the production, to the individual personalities of every member of the Wu-Tang. Truly the pinnacle of hip-hop. ");
 
         Review summerTime = new Review(2L,"Summertime '06",
                 "Summertime '06 is the debut studio double album by American rapper Vince Staples.",
-                "2015","Hip-hop", "/resources/Images/summertime.jpg",
+                "2015","Hip-hop", "/Images/summertime.jpg",
                 "A biological wonder of the world is the warning that mice give each other when they smell the pheromones of predator. It is said that a mouse can scream for hours to let others know about the fear of approaching danger. Summertime 06’ is this same cry from Vince Staples. A young mind letting people know the brutal conditions of just surviving in the hardest parts of Long Beach. Every party song is followed by a track to remind you how different life can be for people like him.");
 
         Review strays = new Review(3L,"Strays With Rabies",
                 "Strays With Rabies is the second studio album by EarthGang",
-                "2015","Rap", "/resources/Images/Strays.jpg",
+                "2015","Rap", "/Images/Strays.jpg",
                 "It’s hard to describe this one, but every song is an ear-worm. You’ll want to listen to every track on this album over and over again. Not only are the songs fun to listen too but thought provoking as well. Each track is accompanied by an equally great music video as well.  ");
 
         Review iDontLike = new Review(4L,"I Don't Like Shit, I Don't Go Outside",
                 "I Don't Like Shit, I Don't Go Outside is the second studio album by American rapper Earl Sweatshirt",
-                "2015", "Hip-hop", "/resources/images/earl_sweatshirt.jpg",
+                "2015", "Hip-hop", "/images/earl_sweatshirt.jpg",
                 "Proven perfection. Earl Sweatshirt is establishing his lyrical skill in all aspects from creativity to delivery. This is him telling the world that he has been very meticulous on what he has to say, he’s cementing his thoughts not only about his own life, but the decisions and actions of those around him. Listening through I Don’t Like Shit I don’t Go outside is akin to reading an isolated monk’s writings.");
 
         Review takeMeTo = new Review(5L, "Take Me To Your Leader",
@@ -39,22 +39,22 @@ public class ReviewRepository {
 
         Review liquidSwords = new Review(6L, "Liquid Swords",
                 "Liquid Swords is the second solo studio album by American rapper and Wu-Tang Clan member GZA",
-                "1995", "East Coast Hip-hop", "resources/Images/liquid.jpeg",
+                "1995", "East Coast Hip-hop", "/Images/liquid.jpeg",
                 "Is there anything I can say about this album that hasn’t been said already? Some regard this as, not only the greatest hip-hop album of all time, but as the greatest album of all time. Do I agree? I cannot say, but it is absolutely phenomenal.");
 
         Review betterOffDead = new Review(7L, "BetterOffDEAD",
                 "BetterOffDEAD is the second mixtape by American hip hop trio Flatbush Zombies.",
-                "2013", "Hip-hop", "/resources/Images/betterOff.jpg",
+                "2013", "Hip-hop", "/Images/betterOff.jpg",
                 "The catchiness of BetterOffDEAD is unmatched. From front to back I, personally, have this album memorized. Flatbush Zombies are establishing themselves as ahead of all others.");
 
         Review atrocityExhibition = new Review(8L, "Atrocity Exhibition",
                 "Atrocity Exhibition is the fourth studio album by American rapper Danny Brown",
-                "2016", "Experimental Hip-hop", "/resources/Images/Danny.jpg",
+                "2016", "Experimental Hip-hop", "/Images/Danny.jpg",
                 "The title fits perfectly for this album. It starts with Downward Spiral, the confusion you feel abut this song: should you be ready to listen to another Danny Brown club record, or should you be as tormented as he is? Every feature is a superstar that perfectly demonstrates the energy one can reflect when Danny Brown is preforming with them. ");
 
         Review pinata = new Review(9L, "Piñata",
                 "Piñata is the first collaborative studio album by American rapper Freddie Gibbs and record producer Madlib",
-                "2014", "Hip-hop", "/resources/Images/pinata.jpg",
+                "2014", "Hip-hop", "/Images/pinata.jpg",
                 "Have you been to Gary Indiana? Have you lived in the absolute poorest parts of the Midwest? This album is the closest you can come to experiencing this without actually living through it. The final track, the namesake of the album, is the greatest collaboration project of talent that this genre has ever seen.");
 
 
